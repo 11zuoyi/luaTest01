947541da4e79383918d804c7915a2b9e57e5d21d
zj <575559379@qq.com>
2021-07-08 15:52:14 +0800

    测试idea中使用git

---
+++ src/lua/test01.java
@@ -0,0 +1,9 @@
+package lua;
+
+/**
+ * @author zj
+ * @describe
+ * @create 2021-07-08-15:47
+ */
+public class test01 {
+}
---
+++ src/lua/test02.java
@@ -0,0 +1,10 @@
+package lua;
+
+/**
+ * @author zj
+ * @describe
+ * @create 2021-07-08-15:47
+ */
+public class test02 {
+    private int a;
+}
